f641531eaeedda95185b6453e25f61d5112a0240
miguelgabriel01 <mgbs@discente.ifpe.edu.br>
2022-11-27 13:06:55 -0300

    refactor: ajuste de interface com o usuario na alteração das vagas

--- pub.py
+++ pub.py
@@ -24,9 +24,11 @@ client_id = f'python-mqtt-{random.randint(0, 1000)}'
 def connect_mqtt():
     def on_connect(client, userdata, flags, rc):
         if rc == 0:
-            print("Connected to MQTT Broker!")
+            print(" ")
+           # print("Connected to MQTT Broker!")
         else:
-            print("Failed to connect, return code %d\n", rc)
+            print(" ")
+            #print("Failed to connect, return code %d\n", rc)
 
     client = mqtt_client.Client(client_id)
     client.username_pw_set('emqx', 'public')
@@ -39,51 +41,77 @@ dadosSobreAsVagas = [
         "id": 1,
         "situacao": "ocupada",
         "dataDaUltimaAtualizacao":  dateInText,
-        "horaDaUltimaAtualizacao":  updateTime
+        "horaDaUltimaAtualizacao":  updateTime,
+        "localizacao": "shopping north way"
     },
     {
         "id": 2,
         "situacao": "livre",
         "dataDaUltimaAtualizacao":  dateInText,
-        "horaDaUltimaAtualizacao":  updateTime
+        "horaDaUltimaAtualizacao":  updateTime,
+        "localizacao": "shopping north way"
     },   
     {
         "id": 3,
         "situacao": "livre",
         "dataDaUltimaAtualizacao":  dateInText,
-        "horaDaUltimaAtualizacao":  updateTime    
+        "horaDaUltimaAtualizacao":  updateTime,
+        "localizacao": "shopping north way"
     },    
     {
         "id": 4,
         "situacao": "livre",
         "dataDaUltimaAtualizacao":  dateInText,
-        "horaDaUltimaAtualizacao":  updateTime    
+        "horaDaUltimaAtualizacao":  updateTime,
+        "localizacao": "shopping north way"   
     },    
     {
         "id": 5,
         "situacao": "ocupada",
         "dataDaUltimaAtualizacao":  dateInText,
-        "horaDaUltimaAtualizacao":  updateTime    
+        "horaDaUltimaAtualizacao":  updateTime,
+        "localizacao": "shopping north way"    
     }
 ]
         
+totalParkingSpaces = len(dadosSobreAsVagas)
+
 def publish(client):    
+      print('-----------------------------')
+      print("*       Smarth-parking      *")
+      print('-----------------------------')
+
       while True:
         numeroVaga = 0
         time.sleep(1)
-        
+         
         result = client.publish(topic, f"{dadosSobreAsVagas}")
         status = result[0]
         if status == 0:
+           print(" ")
+           print("Numero total de vagas no estacionamento: " + str(totalParkingSpaces))
+           print(" ")
+           print("Status atual sobre as vagas: ")
+           print(" ")
            print(dadosSobreAsVagas)
+           print("    ")
+           print("    ")
            idSelect =  int(input("Digite a vaga que deseja alterar: "))
 
            if(idSelect):
             print("    ")
-            print("    ")
-            print("    ")
-            dadosSobreAsVagas[idSelect-1]["situacao"] = "ocupada"
-                
+            
+            print("Situação atual da vaga: " + dadosSobreAsVagas[idSelect-1]["situacao"])
+            print("Deseja alterar o status desta vaga?")
+            print("1 - Sim / 2 - Não ")
+            idSelectStatus =  int(input())
+
+            if idSelectStatus == 1:
+                if  dadosSobreAsVagas[idSelect-1]["situacao"] == "ocupada":
+                    dadosSobreAsVagas[idSelect-1]["situacao"] = "livre"
+                else:
+                    dadosSobreAsVagas[idSelect-1]["situacao"] = "ocupada"
+
         else:
             print(f"Falhou ao enviar mensagem para o topico {topic}")
         numeroVaga += 1
